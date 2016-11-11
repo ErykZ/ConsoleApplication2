9c39f2c5f7e75071a65dac331d5c120a2f5118ba
ja <ja@JA-KOMPUTER>
2016-11-11 13:04:31 +0100

    test1

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test hello world2");
+            Console.WriteLine("test hello world3");
             Console.Read();
         }
     }
